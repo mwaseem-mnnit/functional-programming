6d979d32dccd7f935ede0f566348030d61076db2
mohd.waseem <mohd.waseem@rivigo.com>
2020-03-20 21:58:38 +0530

    Main file, Hello World

---
+++ src/main/java/com/mwaseem/fp/Main.java
@@ -0,0 +1,10 @@
+package com.mwaseem.fp;
+
+/**
+ * Created by mohd.waseem on 20/03/20.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!");
+    }
+}
